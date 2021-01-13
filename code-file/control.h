0c48b2a93ab169a988df3c2db1e4ef494a875f60
Zhang7391 <uhoo.tw60@gmail.com>
2021-01-13 08:23:46 -0500

    商店、玩家背包功能正式啟用

--- code-file/control.h
+++ code-file/control.h
@@ -2,40 +2,73 @@
 #define Control_H
 
 #include<iostream>
+#include<fstream>
 #include<conio.h>
 #include<string>
 #include<ctime>
 
 #define UNI 10	//Fareland Number Maximum
+#define PAR 60	//Store Merchandise & Backpack Number Maximum
 
 using namespace std;
 
 class Game
 {
 private:
+	void MainPage(int);
+	void ShopPage(int);
 
-public:
-
-};
-
-class Page
-{
-private:
 	int FarmlandChose = 0;
+	int ShopChose = 0;
+	int SubShopPageNow = 0;
+
 public:
-	void MainInIt(int, string);
-	void main(int);
+	void RefreshMain(void);
+	void MainInIt(void);
+
+	void RefreshShop(void);
+	void ShopInIt(void);
+
+	int NowPlace = 0;
+	/* Place Code:
+	 * Main: 0
+	 * Shop: 1
+	 * End Game: 99 */
 };
 
-class AllValue	//just have the game global value
+class Farmland	//just have the game global value
 {
 public:
-	int FarmlandNumber = 10;
-	struct FramlandAttributes
-	{
-		string status = "idle";
-		int time;
-		string chose;
-	}FarmlandInfo[UNI];	//Farmland Number Maximum
+	int FarmlandNumber = 1;
+
+	string *status = new string[UNI]();
+	int *time = new int[UNI]();
+	string *chose = new string[UNI]();
 };
+Farmland FLvalue;
+
+class StoreMerchandise
+{
+public:
+	string *Name = new string[PAR]();
+	int *SellPrice = new int[PAR]();
+	int *AcquisitionPrice = new int[PAR]();
+	string *chose = new string[PAR]();
+
+	int HowMuchMerchandise = 0;
+};
+StoreMerchandise Merchandise;
+
+class BackPack
+{
+public:
+	/* store merchandise information */
+	string *SName = new string[PAR]();
+	int *SHaveNumber = new int[PAR]();
+
+	/* other */
+	string Name[0];
+	int HaveNumber[0];
+};
+BackPack push;
 #endif
--- code-file/gaming.cpp
+++ code-file/gaming.cpp
@@ -2,37 +2,164 @@
 
 int main()
 {
-	Page view;
-	AllValue value;
+	Game view;
 
-	string ShowFull("Welcome to this large farmland!");
-	int ShowFullLong = ShowFull.length();
-
-	view.MainInIt(ShowFullLong, ShowFull);
-	view.main(300);
-
+	view.MainInIt();
+	view.ShopInIt();
+Main:	//Place Code 0
 	while(1)
 	{
-		if(kbhit())
-		{
-			view.main(getch());
-		}
-		else view.main(300);
+		view.RefreshMain();
+		if(view.NowPlace == 1) goto Shop;
+		if(view.NowPlace == 99) goto EndGame;
 	}
 
+Shop:	//Place Code 1
+	while(1)
+	{
+		view.RefreshShop();
+		if(view.NowPlace == 0) goto Main;
+		if(view.NowPlace == 99) goto EndGame;
+	}
+
+EndGame:	//Place Code 99
+	cout << '\n';
 	return 0;
 }
 
-//Page private
+//Game private
+
+/* When loading window in the end.
+ * This window will keep running.
+ * Such that player home page.
+ * Display the player's current status. */
+void Game::MainPage(int control)
+{
+	system("clear");
+	system("clear");
+
+	if('9' >= control && control >= '0')
+	{
+		if(control == '0') control += 10;
+		if(FLvalue.FarmlandNumber > control - 49)
+		{
+			FLvalue.chose[this->FarmlandChose] = "";
+			this->FarmlandChose = control - 49;
+		}
+	}
+	FLvalue.chose[this->FarmlandChose] = "<-";
+
+	cout << "My farmland" << "\n\n";
+	cout << "Farmland Name\t\tStatus\t\tTime\t\n";
+	for(int a=0;FLvalue.FarmlandNumber>a;a++)
+	{
+		cout << "Farmland NO." << a+1 << "\t\t" << FLvalue.status[a];
+		if(FLvalue.status[a] == "idle") cout << "\t\t-:-:-\t";
+		else cout << "\t";	//not finish!!!!!
+		cout << FLvalue.chose[a] << '\n';
+	}
+
+	cout << "\nWhat are you want to go?\n";
+	cout << "    Shop(S)    Quit(Q)\n\n\n";
+
+	cout << "Instructions:\n";
+	cout << "  1.Keyboard number keys select the farmland.\n";
+
+	cout << "\nRemark: Plase wait screen refreshed, then enter the next command.\n";
+
+	if(control == 300) system("sleep 1s");
+}
+
+void Game::ShopPage(int control)
+{
+	system("clear");
+	system("clear");
+
+	if(control == 'N' || control == 'n')
+		if(Merchandise.HowMuchMerchandise > this->SubShopPageNow + 10) this->SubShopPageNow += 10;
+
+	if(control == 'B' || control == 'b')
+		if(this->SubShopPageNow - 10 >= 0) this->SubShopPageNow -= 10;
+
+	if('9' >= control && control >= '0')
+	{
+		if(control == '0') control += 10;
+		for(int a=0;PAR>a;a+=10) Merchandise.chose[this->ShopChose + a] = "";
+		this->ShopChose = control - 49;
+	}
+	if(this->SubShopPageNow + 10 > Merchandise.HowMuchMerchandise)
+	{
+		for(int a=0;PAR>a;a+=10) Merchandise.chose[this->ShopChose + a] = "";
+		if(this->ShopChose > Merchandise.HowMuchMerchandise % 10)
+			this->ShopChose =  Merchandise.HowMuchMerchandise % 10 - 1;
+	}
+	for(int a=0;PAR>a;a+=10) Merchandise.chose[this->ShopChose + a] = "<-";
+
+	long unsigned int tab;
+	if(this->SubShopPageNow == 0) tab = 6;
+	else tab = 5;
+
+	cout << "Welcome to Zhang7391's store!\n";
+	cout << "Although it's author store, it's not cheaper.\n\n";
+
+	cout << "Name\t\tSell\tbuy\town\n";
+	for(int a=this->SubShopPageNow;this->SubShopPageNow + 10>a;a++)
+	{
+		if(Merchandise.Name[a] == "") break;
+
+		cout << a+1 << '.' << Merchandise.Name[a];
+		if(Merchandise.Name[a].length() >= tab) cout << "\t";
+		else cout << "\t\t";
+		cout << Merchandise.SellPrice[a] << "\t";
+		cout << Merchandise.AcquisitionPrice[a] << "\t";
+		cout << push.SHaveNumber[a] << "\t";
+		cout << Merchandise.chose[a] << '\n';
+	}
+	cout << "\t\tPage " << this->SubShopPageNow / 10 + 1 << '/';
+	if(Merchandise.HowMuchMerchandise % 10 == 0) cout << Merchandise.HowMuchMerchandise / 10 << '\n';
+	else cout << Merchandise.HowMuchMerchandise / 10 + 1 << '\n';
+
+	cout << "\n   Previous page(B)\tNext Page(N)\n\n";
+
+    cout << "\nWhat are you want to go?\n";
+	cout << "    Home(M)    Quit(Q)\n\n";
+
+	cout << "Instructions:\n";
+	cout << "  1.Keyboard number keys select the merchandise.\n";
+
+	if(control == 300) system("sleep 1s");
+}
+
+//Game public
+
+/* Control display window now.
+ * And keep Refreshing the MainPage. */
+void Game::RefreshMain(void)
+{
+	if(kbhit())
+	{
+		int control = getch();
+
+		if(control == 's' || control == 'S') this->NowPlace = 1;
+		if(control == 'q' || control == 'Q') this->NowPlace = 99;
+
+		this->MainPage(control);
+	}
+	else this->MainPage(300);
+}
 
 /* When player first opening this game.
  * First welcome player's window. */
-void Page::MainInIt(int JumpTime, string ShowFull)
+void Game::MainInIt(void)
 {
+	string ShowFull("Welcome to this large farmland!");
+	int JumpTime = ShowFull.length();
 	int Jump = 0;
+
+	for(int a=0;UNI>a;a++) FLvalue.status[a] = "idle";
+
 	while(JumpTime >= Jump)
 	{
-		AllValue value;
 		system("clear");
 		system("clear");
 
@@ -44,8 +171,8 @@ void Page::MainInIt(int JumpTime, string ShowFull)
 
 		cout << "My farmland" << "\n\n";
 
-		cout << "Farmland Name\t\tStatus\t\tTime\t\t\n\n";
-		for(int a=0;value.FarmlandNumber>a;a++) cout << "Farmland NO." << a+1 << "\t\tloading...\tloading...\n";
+		cout << "Farmland Name\t\tStatus\t\tTime\t\t\n";
+		for(int a=0;FLvalue.FarmlandNumber>a;a++) cout << "Farmland NO." << a+1 << "\t\tloading...\tloading...\n";
 
 		cout << "\n" << ShowUp << '\n';	//jump text
 		cout << ShowDown << "\n";
@@ -55,43 +182,39 @@ void Page::MainInIt(int JumpTime, string ShowFull)
 	}
 }
 
-//Page public
-
-/* When loading window in the end.
- * This window will keep running.
- * Such that player home page.
- * Mark the player's current status. */
-void Page::main(int control)
+/* 1.Control display window now.
+ * 2.keep Refreshing the Store Page. */
+void Game::RefreshShop(void)
 {
-	AllValue value;
-	system("clear");
-	system("clear");
-
-	if(57 >= control && control >= 48)
+	if(kbhit())
 	{
-		if(control == 48) control += 10;
-		if(value.FarmlandNumber > control - 49)
-		{
-			value.FarmlandInfo[this->FarmlandChose].chose = "";
-			this->FarmlandChose = control - 49;
-		}
-	}
-	value.FarmlandInfo[this->FarmlandChose].chose = "<-";
+		int control = getch();
 
-	cout << "My farmland" << "\n\n";
-	cout << "Farmland Name\t\tStatus\t\tTime\t\n\n";
-	for(int a=0;value.FarmlandNumber>a;a++)
+		if(control == 'M' || control == 'm') this->NowPlace = 0;
+		if(control == 'Q' || control == 'q') this->NowPlace = 99;
+
+		this->ShopPage(control);
+	}
+	else this->ShopPage(300);
+}
+
+/* 1.When game initialization, loading the store merchandise information. 
+ * 2.Initialization the backpack information. */
+void Game::ShopInIt(void)
+{
+	string clear;
+	ifstream ui;
+	ui.open("store_merchandise");
+
+	while(getline(ui, Merchandise.Name[Merchandise.HowMuchMerchandise]))
 	{
-		cout << "Farmland NO." << a+1 << "\t\t" << value.FarmlandInfo[a].status;
-		if(value.FarmlandInfo[a].status == "idle") cout << "\t\t-:-:-\t";
-		else cout << "\t";	//not finish!!!!!
-		cout << value.FarmlandInfo[a].chose << "\n";
+		ui >> Merchandise.SellPrice[Merchandise.HowMuchMerchandise];
+		ui >> Merchandise.AcquisitionPrice[Merchandise.HowMuchMerchandise] >> clear;
+
+		push.SName[Merchandise.HowMuchMerchandise] = Merchandise.Name[Merchandise.HowMuchMerchandise];
+		push.SHaveNumber[Merchandise.HowMuchMerchandise] = 0;
+
+		ui.ignore();
+		Merchandise.HowMuchMerchandise += 1;
 	}
-
-	cout << "\nInstructions:\n";
-	cout << "  1.Keyboard number keys select the farmland.\n";
-
-	cout << "\nRemark: Plase wait screen refreshed, then enter the next command.\n";
-
-	if(control == 300) system("sleep 1s");
 }
